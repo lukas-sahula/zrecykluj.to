dff559ec78bc3134a58efac2e81d288ca5cab5c8
konviczka <lerljaku@gmail.com>
2021-01-25 22:30:25 +0100

    init home navigation page

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     kotlin("android")
     id("kotlin-android-extensions")
+    id("kotlin-android")
 }
 
 group = "me.jakub"
@@ -12,6 +13,12 @@ dependencies {
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    implementation("com.google.android.material:material:1.2.0-alpha04")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
 }
 
 android {
--- androidApp/src/main/java/me/jakub/androidApp/MainActivity.kt
+++ androidApp/src/main/java/me/jakub/androidApp/MainActivity.kt
@@ -13,8 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
     }
 }
