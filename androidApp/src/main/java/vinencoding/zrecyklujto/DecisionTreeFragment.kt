01ae318e189784142bf2661c33adf8c408ca7562
konviczka <lerljaku@gmail.com>
2021-02-16 20:22:12 +0100

    kickoff generating descision tree fragment

--- androidApp/src/main/java/vinencoding/zrecyklujto/DecisionTreeFragment.kt
+++ androidApp/src/main/java/vinencoding/zrecyklujto/DecisionTreeFragment.kt
@@ -1,5 +1,11 @@
 package vinencoding.zrecyklujto
 
+import android.content.Context
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import vinencoding.zrecyklujto.R
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,6 +17,7 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
 /**
  * A simple [Fragment] subclass.
  * Use the [DecisionTreeFragment.newInstance] factory method to
@@ -20,6 +27,12 @@ class DecisionTreeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val recycling_codes = arrayOf(
+        "1;PET;Polyethylentereftalát (PET, PETE);kontejner na plasty, kontejner na PET;Pl",
+        "2;PE-HD;Vysokohustotní (lineární) polyetylen (HDPE, PE-HD);kontejner na plasty;Pa",
+        //....
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +46,33 @@ class DecisionTreeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_decision_tree, container, false)
+        val view = inflater.inflate(R.layout.fragment_decision_tree, container, false)
+
+        val cardLinearLayout = view.findViewById<LinearLayout>(R.id.cardsLayout)
+
+        for (code in recycling_codes){
+            val parts = code.split(";").toTypedArray();
+
+            val numCode = parts[0]
+            val strCode = parts[1]
+            val header = parts[2]
+            val description = parts[3]
+            var type = parts[4]
+
+            val card = CardView(view.context, null, R.style.BaseCardView)
+            val textView = TextView(card.context)
+
+            textView.text = numCode
+            if (type == "Pl")
+                card.setCardBackgroundColor(R.color.colorYellowPlastic)
+            else if (type == "Pa")
+                card.setCardBackgroundColor(R.color.colorBluePaper)
+
+            card.addView(textView)
+            cardLinearLayout.addView(card)
+        }
+
+        return view
     }
 
     companion object {
